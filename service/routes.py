a16be59ab5d3aa10e962ade345b3345c7e158aa8
Niroshan-Vidanagamage <niroshanvidanagamage@gmail.com>
2026-04-16 23:00:27 +0530

    Format code and add whitespace for readability

--- service/routes.py
+++ service/routes.py
@@ -9,19 +9,19 @@ from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
-
 ############################################################
 # Health Endpoint
 ############################################################
+
 @app.route("/health")
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
 
-
 ######################################################################
 # GET INDEX
 ######################################################################
+
 @app.route("/")
 def index():
     """Root URL response"""
@@ -34,10 +34,10 @@ def index():
         status.HTTP_200_OK,
     )
 
-
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
+
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
@@ -60,6 +60,7 @@ def create_accounts():
     ######################################################################
     # LIST ALL ACCOUNTS
     ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -94,10 +95,10 @@ def get_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
     ######################################################################
     # UPDATE AN EXISTING ACCOUNT
     ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -115,10 +116,10 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
 ######################################################################
     # DELETE AN ACCOUNT
     ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -136,7 +137,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
