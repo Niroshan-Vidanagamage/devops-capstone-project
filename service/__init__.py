61c4a339905a0ddfe0be435436bba36ebe206451
Niroshan Vidanagamage <niroshanvidanagamage@gmail.com>
2026-06-09 21:56:00 +0530

    node.js update for build02

--- service/__init__.py
+++ service/__init__.py
@@ -15,7 +15,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object(config)
 
-#create instance of Talisman
+# create instance of Talisman
 talisman = Talisman(app)
 CORS(app)
 
